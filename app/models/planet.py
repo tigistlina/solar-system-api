130caac908a2254f06e337543dafbf2823bceee1
Tigist Woldearegai <tigistlina@gmail.com>
2023-04-28 14:42:23 -0500

    create Planet model table

---
+++ app/models/planet.py
@@ -0,0 +1,13 @@
+from app import db
+
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    parent_planet = db.Column(db.String)
+    habitable = db.Column(db.Boolean)
+    gravity = db.Column(db.String)
+    nickname = db.Column(db.String)
