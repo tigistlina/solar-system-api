ef68c3d0a62d70007df1ee5b2584d77de0596ee2
Valerie Valentin <valerie.valentine93@gmail.com>
2023-04-28 18:21:20 -0700

    Finished handle planet endpoint

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,7 +1,6 @@
 from app import db
 
 
-
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,60 @@
-from flask import Blueprint, jsonify, abort, make_response
-
-class Planet:
-    def __init__(self, id, name, description, size, parent_planet, habitable, gravity, nickname):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.size = size
-        self.parent_planet = parent_planet
-        self.habitable = habitable
-        self.gravity = gravity
-        self.nickname = nickname
-
-    def create_dict(self):
-        return dict(
-            id=self.id,
-            name=self.name,
-            description=self.description,
-            size=self.size,
-            parent_planet = self.parent_planet,
-            habitable = self.habitable,
-            gravity = self.gravity,
-            nickname = self.nickname
-
-        )
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def handle_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+            name=request_body["name"]
+            description=request_body["description"],
+            size=request_body["size"],
+            parent_planet =request_body["parent_planet"],
+            habitable =request_body["habitable"],
+            gravity =request_body["gravity"],
+            nickname =request_body["name"],
+    )
+
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+
+# from flask import Blueprint, jsonify, abort, make_response
+
+# class Planet:
+#     def __init__(self, id, name, description, size, parent_planet, habitable, gravity, nickname):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.size = size
+#         self.parent_planet = parent_planet
+#         self.habitable = habitable
+#         self.gravity = gravity
+#         self.nickname = nickname
+
+#     def create_dict(self):
+#         return dict(
+#             id=self.id,
+#             name=self.name,
+#             description=self.description,
+#             size=self.size,
+#             parent_planet = self.parent_planet,
+#             habitable = self.habitable,
+#             gravity = self.gravity,
+#             nickname = self.nickname
+
+#         )
+
+
+
+
+
 
 # planets = [
 #     Planet(1, "Earth", "habitable", "12742km in diameter"),
