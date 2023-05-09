d099027b5b3d57f2eebb168e97a31f7d08f06f56
Tigist Woldearegai <tigistlina@gmail.com>
2023-05-08 19:56:57 -0500

    creates moon model

---
+++ app/models/moon.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Moon(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    size = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    mass = db.Column(db.String, nullable=False)
+    # planets = db.relationship("Planet", back_populates="moon")
--- moon.py
+++
@@ -1,3 +0,0 @@
-# from app import db
-
-# class Moon(db.Model):
